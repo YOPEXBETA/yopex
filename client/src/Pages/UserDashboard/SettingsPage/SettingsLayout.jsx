d3e16b49277622fa4b652caf35d9ff33efbe6433
Mouhib Touaiti <mouhib.touaiti@esprit.tn>
2023-08-15 17:25:33 +0100

    updates

--- client/src/Pages/UserDashboard/SettingsPage/SettingsLayout.jsx
+++ client/src/Pages/UserDashboard/SettingsPage/SettingsLayout.jsx
@@ -1,5 +1,3 @@
-import { Grid } from "@mui/material";
-import { makeStyles } from "@mui/styles";
 import React from "react";
 import Billing from "./AccountSettings/Billing";
 import GeneralInformations from "./AccountSettings/GeneralInformations";
@@ -7,103 +5,37 @@ import Privacy from "./AccountSettings/Privacy";
 import HorizantalSettingsTab from "./AccountSettings/SettingTabs/HorizantalSettingsTab";
 import VerticalSettingsTab from "./AccountSettings/SettingTabs/VerticalSettingsTab";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    padding: "0px 5%",
-    [theme.breakpoints.down("lg")]: {
-      padding: "0px 5%",
-    },
-    [theme.breakpoints.down("md")]: {
-      padding: "0px 2%",
-    },
-    [theme.breakpoints.down("sm")]: {
-      padding: "0px 2%",
-    },
-    [theme.breakpoints.down("xs")]: {
-      padding: "0px 0%",
-    },
-  },
-  gridItem: {
-    padding: "0px 6rem",
-    paddingTop: "2rem",
-    [theme.breakpoints.down("lg")]: {
-      padding: "0px 3rem",
-      paddingTop: "2rem",
-    },
-    [theme.breakpoints.down("md")]: {
-      padding: "0px 3rem",
-      paddingTop: "2rem",
-    },
-    [theme.breakpoints.down("sm")]: {
-      padding: "0px 0rem",
-      paddingTop: "2rem",
-    },
-    [theme.breakpoints.down("xs")]: {
-      padding: "0px 0rem",
-      paddingTop: "2rem",
-    },
-  },
-  VerticalTabItem: {
-    padding: "0px 2rem",
-    [theme.breakpoints.down("lg")]: {
-      padding: "0px 2rem",
-    },
-    [theme.breakpoints.down("md")]: {
-      padding: "0px 2rem",
-    },
-    [theme.breakpoints.down("sm")]: {
-      padding: "0px 0rem",
-    },
-    [theme.breakpoints.down("xs")]: {
-      padding: "0px 0rem",
-    },
-  },
-}));
-
 const SettingsLayout = () => {
-  const classes = useStyles();
   const [value, setValue] = React.useState(0);
   const changeValue = (e, params) => setValue(params);
 
   return (
-    <div>
-      <Grid container className={classes.root}>
-        <Grid
-          item
-          lg={3}
-          xs={3}
-          sx={{ display: { lg: "block", md: "bloc", sm: "block", xs: "none" } }}
-        >
+    <div className="px-2 md:px-5 lg:px-5">
+      <div className="grid grid-cols-12 lg:gap-8 gap-0">
+        <div className="col-span-3 hidden lg:block md:hidden">
           <VerticalSettingsTab changeValue={changeValue} value={value} />
-        </Grid>
+        </div>
 
-        <Grid
-          item
-          lg={9}
-          xs={9}
-          className={classes.VerticalTabItem}
-          mt={2}
-          sx={{ display: { lg: "none", md: "none", sm: "none", xs: "block" } }}
-        >
+        <div className="col-span-12 mt-2 block lg:hidden md:block">
           <HorizantalSettingsTab changeValue={changeValue} value={value} />
-        </Grid>
+        </div>
 
         {value === 0 && (
-          <Grid item lg={9} md={9} sm={9} xs={12} className={classes.gridItem}>
+          <div className="col-span-12 md:col-span-9  mt-8 lg:px-16 px-2">
             <GeneralInformations />
-          </Grid>
+          </div>
         )}
         {value === 1 && (
-          <Grid item lg={9} md={9} sm={9} xs={12} className={classes.gridItem}>
+          <div className="col-span-12 md:col-span-9  mt-8 px-16">
             <Privacy />
-          </Grid>
+          </div>
         )}
         {value === 2 && (
-          <Grid item lg={9} md={9} sm={9} xs={12} className={classes.gridItem}>
+          <div className="col-span-12 md:col-span-9  mt-8 px-16">
             <Billing />
-          </Grid>
+          </div>
         )}
-      </Grid>
+      </div>
     </div>
   );
 };
