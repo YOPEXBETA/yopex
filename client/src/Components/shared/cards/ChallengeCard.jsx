0bde26ca53f3b3eb985b75d65a7d8de644674c26
Mouhib Touaiti <mouhib.touaiti@esprit.tn>
2023-08-28 19:50:41 +0100

    update challenge card ui

--- client/src/Components/shared/cards/ChallengeCard.jsx
+++ client/src/Components/shared/cards/ChallengeCard.jsx
@@ -1,43 +1,10 @@
-import StarIcon from "@mui/icons-material/Star";
-import {
-  Button,
-  Card,
-  Divider,
-  Rating,
-  Stack,
-  Typography,
-} from "@mui/material";
-import { makeStyles, styled } from "@mui/styles";
 import React from "react";
-import { Link, useParams } from "react-router-dom";
+import StarIcon from "@mui/icons-material/Star";
+import { Rating } from "@mui/material";
+import { Link } from "react-router-dom";
 import getDeadlineDifference from "./../../getDeadlineDifference";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-    marginBottom: theme.spacing(2),
-  },
-  content: {
-    flex: "1 0 auto",
-  },
-  cover: {
-    width: 120,
-    height: 120,
-    margin: theme.spacing(2),
-  },
-  icon: {
-    marginRight: theme.spacing(0.5),
-  },
-  chip: {
-    marginRight: theme.spacing(1),
-    marginBottom: theme.spacing(1),
-    borderRadius: "15px",
-  },
-}));
-
 const ChallengeCard = ({ challenge }) => {
-  const classes = useStyles();
-
   const isChallengeInProgress = (challenge) => {
     if (
       getDeadlineDifference(challenge?.deadline) === "0 Days 0 Hours 0 Minutes"
@@ -48,74 +15,57 @@ const ChallengeCard = ({ challenge }) => {
 
   return (
     <div>
-      <Divider />
       <Link to={`/browse/contestDetails/${challenge._id}`}>
         <div className="shadow-md border-green-500 border-b-2 rounded-lg flex hover:scale-105">
           <img
             src={challenge.company?.companyLogo}
             className="object-cover  bg-gray-400 h-40 w-40"
           />
-          <div className={classes.content}>
+          <div>
             <div className=" p-5">
-              <Stack spacing={2}>
+              <div className="space-y-4">
                 <div>
                   <h5 className=" text-lg font-semibold mb-2">
                     {challenge.title}
                   </h5>
-                  <p
-                    variant="body1"
-                    color="textSecondary"
-                    whiteSpace={"normal"}
-                  >
-                    {challenge.description}
-                  </p>
+                  <p className="truncate w-96">{challenge.description}</p>
                 </div>
 
-                <Stack
-                  flexDirection={"row"}
-                  alignItems={"flex-end"}
-                  columnGap={"1rem"}
-                >
-                  <Stack
-                    display={"flex"}
-                    flexDirection={"row"}
-                    alignItems={"flex-end"}
-                  >
+                <div className=" flex items-end gap-4">
+                  <div className="flex items-end">
                     <Rating
                       name={`rating-${challenge._id}`}
                       value={challenge.totatlStars}
                       precision={0.5}
                       readOnly
-                      icon={<StarIcon className={classes.icon} />}
+                      icon={<StarIcon />}
                     />
                     <p>({challenge.totalStars})</p>
-                  </Stack>
+                  </div>
 
-                  <Stack flexDirection={"row"} columnGap={"0.3rem"}>
+                  <div className="flex gap-1">
                     <p className="text-green-500 font-bold">
-                      {challenge.price}$
+                      {challenge.price} XP
                     </p>
                     <p className="text-green-500">Prize</p>
-                  </Stack>
-                  <Stack flexDirection={"row"} columnGap={"0.3rem"}>
-                    <Typography variant="subtitle1">
-                      {challenge.users.length}
-                    </Typography>
+                  </div>
+                  <div className="flex gap-1">
+                    <p className="font-bold">{challenge.users.length}</p>
                     <p>Participants</p>
-                  </Stack>
-                  <Button
-                    color={
-                      isChallengeInProgress(challenge) ? "success" : "error"
-                    }
-                    className={classes.gridItem}
-                    style={{ padding: 0 }}
+                  </div>
+                  <button
+                    className={`${
+                      isChallengeInProgress(challenge)
+                        ? "text-green-500"
+                        : "text-red-500"
+                    }  p-0`}
                   >
                     {isChallengeInProgress(challenge)
                       ? "In Progress"
                       : "Finished"}
-                  </Button>
-                </Stack>
-              </Stack>
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
