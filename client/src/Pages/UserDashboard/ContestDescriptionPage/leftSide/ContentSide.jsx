447e2c3cd015628f3223328f2ea5e72ac23502ea
Mouhib Touaiti <mouhib.touaiti@esprit.tn>
2023-09-01 22:32:38 +0100

    updates ui

--- client/src/Pages/UserDashboard/ContestDescriptionPage/leftSide/ContentSide.jsx
+++ client/src/Pages/UserDashboard/ContestDescriptionPage/leftSide/ContentSide.jsx
@@ -9,7 +9,7 @@ const ContentSide = ({ value, changeValue }) => {
     <div className="space-y-2">
       {value === 0 && (
         <div className="grid grid-cols-12 gap-2">
-          <div className="lg:col-span-8 md:col-span-8 sm:col-span-12 col-span-12">
+          <div className="lg:col-span-8 md:col-span-8 sm:col-span-12 col-span-12 lg:pr-6">
             <TasksDescription />
           </div>
           <div className="hidden lg:block md:block lg:col-span-4 md:col-span-4 sm:col-span-12 col-span-12">
