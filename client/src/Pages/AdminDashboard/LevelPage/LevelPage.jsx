6b8d9b1b1ccefe99fa5ec7238bbed4b48982c533
saifmessaoudi <saifmessaoudi99@gmail.com>
2023-09-04 17:28:31 +0100

    Fix add new level with alert confirmation

--- client/src/Pages/AdminDashboard/LevelPage/LevelPage.jsx
+++ client/src/Pages/AdminDashboard/LevelPage/LevelPage.jsx
@@ -1,21 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import { useCreateLevel , useGetLevels } from "../../../hooks/react-query/useLevels";
-import { Grid, Stack } from "@mui/material";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
+
 
 const LevelPage = () => {
 
-  const { mutate } =useCreateLevel() 
-  const {data } = useGetLevels()
-  console.log(data);
+  const { mutate } =useCreateLevel();
+  const {data } = useGetLevels();
+  const [showAlert, setShowAlert] = useState(false);
+  const [confirmationMessage, setConfirmationMessage] = useState('');
+
+  const lastLevel  = data && data[data.length-1];
+
+  const handleAddLevel = () => {
+    // Check if there is a last level
+    if (lastLevel) {
+      const newLevelNumber = parseInt(lastLevel.name.replace('Level ', '')) + 1;
+      const message = `A new Level ${newLevelNumber} will be created. Do you want to continue?`;
+
+      // Set the confirmation message
+      setConfirmationMessage(message);
+
+      // Display the custom alert
+      setShowAlert(true);
+    }
+  };
+
+  const handleAlertOK = () => {
+    // Perform the action you want when the user clicks OK
+    mutate();
+
+    // Close the alert
+    setShowAlert(false);
   
+  };
+
+  const handleAlertCancel = () => {
+    // Close the alert without taking any action
+    setShowAlert(false);
+  };
+
+
   return (
     <div>
-  <button onClick={mutate} className="bg-zinc-800 rounded-full mb-2 text-white px-4 py-2 w-1/6" type="submit">
+  <button  onClick={handleAddLevel} className="bg-zinc-800 rounded-full mb-2 text-white px-4 py-2 w-1/6" type="submit">
     Add a new Level
   </button>
+  {showAlert && (
+        <div
+        id="alert-additional-content-5"
+        className="p-4 border border-gray-300 rounded-lg bg-gray-50 dark:border-gray-600 dark:bg-gray-800"
+          role="alert"
+        >
+          <div className="flex items-center">
+            <span className="sr-only">Info</span>
+            <h3 className="text-lg font-medium">Are you sure ?</h3>
+          </div>
+          <div className="mt-2 mb-4 text-sm">
+            {confirmationMessage}
+          </div>
+          <div className="flex">
+            <button
+              type="button"
+              className="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-xs px-3 py-1.5 mr-2 text-center inline-flex items-center dark:bg-gray-600 dark:hover:bg-gray-500 dark:focus:ring-gray-800"
+              onClick={handleAlertOK}
+            >
+              OK
+            </button>
+            <button
+              type="button"
+              className="text-gray-800 bg-transparent border border-gray-700 hover:bg-gray-800 hover:text-white focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-xs px-3 py-1.5 text-center dark:border-gray-600 dark:hover:bg-gray-600 dark:focus:ring-gray-800 dark:text-gray-300 dark:hover:text-white"
+              onClick={handleAlertCancel}
+              aria-label="Close"
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      )}
   <div className="mt-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 ">
     {data?.map(
       (badgeData) =>
@@ -27,9 +88,8 @@ const LevelPage = () => {
       </div>
         )
     )}
-  </div>
-
-  
+   
+</div>
 </div>
  
   )
