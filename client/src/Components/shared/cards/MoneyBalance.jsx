e5035d1987f8e959c3c7af88de57e8856f7e86bd
Mouhib Touaiti <mouhib.touaiti@esprit.tn>
2023-09-18 20:24:35 +0100

    update ui

--- client/src/Components/shared/cards/MoneyBalance.jsx
+++ client/src/Components/shared/cards/MoneyBalance.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { FaGamepad } from "react-icons/fa";
 import { useUserById } from "../../../hooks/react-query/useUsers";
 import { useSelector } from "react-redux";
 
@@ -12,10 +11,12 @@ const MoneyBalance = () => {
       <div className="rounded p-4 py-6 bg-white border-green-500 border-b-2 shadow-md">
         <div className="flex justify-between">
           <h4 className="text-xl font-medium">Balance</h4>
-          <h4 className="text-lg text-primary font-bold text-green-500">
-            {" "}
-            {userProfile?.balance || 0} points
-          </h4>
+          <div className="flex items-center gap-2">
+            <h4 className="text-lg text-primary font-bold text-green-500">
+              {userProfile?.balance || 0}
+            </h4>
+            <p className="text-lg text-green-500 italic font-bold">XP</p>
+          </div>
         </div>
       </div>
     </div>
