2c658067fc494d0a7fff3d36e9df2ae8dfea030d
saifmessaoudi <saifmessaoudi99@gmail.com>
2023-09-22 13:11:42 +0100

    ui Alert delete my company

--- client/src/Pages/UserDashboard/CompanyPage/TopSide/CompanyProfileInformations.jsx
+++ client/src/Pages/UserDashboard/CompanyPage/TopSide/CompanyProfileInformations.jsx
@@ -11,6 +11,24 @@ import { FaUserMinus, FaTrash, FaEdit, FaUserPlus } from "react-icons/fa";
 import { CompanyProfileNavigationTab } from "../../../../Components/Tabs/CompanyProfileNavigationTab";
 
 const CompanyProfileInformations = ({ changeValue, value }) => {
+  const [confirmationDialogOpen, setConfirmationDialogOpen] = useState(false);
+  const handleDeleteClick = () => {
+    // Show the confirmation dialog
+    setConfirmationDialogOpen(true);
+  };
+
+  const handleConfirmDelete = () => {
+    // Perform the delete action
+      handleDeleteCompany();
+
+    // Close the confirmation dialog
+    setConfirmationDialogOpen(false);
+  };
+  const handleCancelDelete = () => {
+    // Close the confirmation dialog without deleting
+    setConfirmationDialogOpen(false);
+  };  
+
   const [openPostModal, setOpenPostModal] = useState(false);
   const { companyId } = useParams();
   const { user } = useSelector((state) => state.auth);
@@ -161,7 +179,7 @@ const CompanyProfileInformations = ({ changeValue, value }) => {
               </a>
               {company && company.user === user._id && (
                 <button
-                  onClick={handleDeleteCompany}
+                  onClick={handleDeleteClick}
                   className="xl:block lg:block md:block cursor-pointer capitalize font-medium hover:scale-105 bg-red-500 p-2 sm:p-4 rounded-lg text-white"
                 >
                   <FaTrash className="w-4 h-4 sm:w-6 sm:h-6" />
@@ -204,6 +222,31 @@ const CompanyProfileInformations = ({ changeValue, value }) => {
           />
         </div>
       </div>
+      {/* Confirmation Dialog */}
+      {confirmationDialogOpen && (
+        <div className="fixed inset-0 flex items-center justify-center z-50">
+          <div className="bg-white p-6 rounded-lg shadow-lg">
+            <h2 className="text-xl font-semibold mb-4">Confirm Deletion</h2>
+            <p className="text-gray-700 mb-4">
+              Are you sure you want to delete this job?
+            </p>
+            <div className="flex justify-end">
+              <button
+                onClick={handleCancelDelete}
+                className="px-4 py-2 text-gray-500 hover:text-gray-700"
+              >
+                Cancel
+              </button>
+              <button
+                onClick={handleConfirmDelete}
+                className="px-4 py-2 ml-2 bg-green-500 text-white hover:bg-green-600"
+              >
+                Confirm
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
