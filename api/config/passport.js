54ccccb9613764627add3725de44efac8181cefc
Mouhib Touaiti <mouhib.touaiti@esprit.tn>
2023-10-10 23:14:20 +0100

    updates backend

--- api/config/passport.js
+++ api/config/passport.js
@@ -13,6 +13,11 @@ passport.use(
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: "http://localhost:8000/auth/google/callback",
+
+      callbackURL:
+        process.env.NODE_ENV === "development"
+          ? "http://localhost:8000/auth/google/callback"
+          : "https://yopex-api.tabaani.co/auth/google/callback",
     },
 
     async (accessToken, refreshToken, profile, cb) => {
