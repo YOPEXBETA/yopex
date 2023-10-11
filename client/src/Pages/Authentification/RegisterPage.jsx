78a49963276999f4c3a3d0b417dfed98f2ebc3bb
Mouhib Touaiti <mouhib.touaiti@esprit.tn>
2023-10-11 13:46:57 +0100

    updates

--- client/src/Pages/Authentification/RegisterPage.jsx
+++ client/src/Pages/Authentification/RegisterPage.jsx
@@ -3,16 +3,18 @@ import Register from "./components/Register";
 
 const RegisterPage = () => {
   return (
-    <div className="grid gap-6 md:grid-cols-2 bg-white">
-      <div className="hidden lg:block md:block col-span-6 md:col-span-1 h-screen sm:hidden">
-        <img
-          src="https://source.unsplash.com/4wOkqiXNP7M"
-          className=" h-screen object-cover"
-        />
-      </div>
+    <div>
+      <div className="grid gap-6 md:grid-cols-2 bg-white">
+        <div className="hidden lg:block md:block col-span-6 md:col-span-1 h-screen sm:hidden">
+          <img
+            src="https://source.unsplash.com/4wOkqiXNP7M"
+            className=" h-screen object-cover"
+          />
+        </div>
 
-      <div className="col-span-6 md:col-span-1 flex flex-col items-center my-16">
-        <Register />
+        <div className="col-span-6 md:col-span-1 flex flex-col items-center my-16">
+          <Register />
+        </div>
       </div>
     </div>
   );
