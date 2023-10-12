7f8b18715d8e1d4a01dc97287257ea460a536e98
Mouhib Touaiti <mouhib.touaiti@esprit.tn>
2023-10-12 18:20:21 +0100

    working on the Seo

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import ScrollTop from "./Components/ScrollTop";
 import Routes from "./routes/index";
 import { useEffect } from "react";
 import { Toaster } from "react-hot-toast";
+import { Helmet } from "react-helmet";
 
 const queryClient = new QueryClient();
 
@@ -24,6 +25,18 @@ const App = () => {
       <QueryClientProvider client={queryClient}>
         <Toaster />
         {/*<ReactQueryDevtools />*/}
+        <Helmet>
+          <title>YOPEX</title>
+          <meta
+            name="description"
+            content="yopex is a social media platform for startups where freelancers and startups can connect"
+          />
+          <meta
+            name="keywords"
+            content="Freelance, YOPEX, Startups, freelancers, Social Media, Internships, Job Opportunities"
+          />
+        </Helmet>
+
         <ScrollTop>
           <Routes />
         </ScrollTop>
