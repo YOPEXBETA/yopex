b392ed849b9b10cdd37c6925e4486cb9528a7bdd
Mouhib Touaiti <mouhib.touaiti@esprit.tn>
2023-10-14 13:37:19 +0100

    update the ui of the social post modal

--- client/src/Components/shared/CreatePost/AddSocialPostModal.jsx
+++ client/src/Components/shared/CreatePost/AddSocialPostModal.jsx
@@ -61,7 +61,31 @@ export const AddSocialPostModal = ({ open, handleClose }) => {
         <div className="bg-transparent absolute inset-0 flex justify-center items-center">
           <div className="bg-white rounded-lg shadow-xl w-full max-w-xl">
             <form onSubmit={handleSubmit(onSubmit)}>
-              <h5 className="text-lg font-bold p-4">Create a Post</h5>
+              <div className="flex justify-between items-center p-4">
+                <h5 className="text-lg font-bold">Create a Post</h5>
+                <button
+                  type="button"
+                  onClick={handleClose}
+                  className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-xs md:text-sm w-7 h-7 md:w-8 md:h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                  data-modal-hide="defaultModal"
+                >
+                  <svg
+                    className="w-3 h-3"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 14"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                    />
+                  </svg>
+                </button>
+              </div>
 
               <div className="px-4">
                 <div className="space-y-4">
@@ -90,6 +114,7 @@ export const AddSocialPostModal = ({ open, handleClose }) => {
                           }
                           value={value}
                           className="w-full border-gray-300 rounded focus:outline-none focus:border-green-500"
+                          placeholder="Select Categories"
                         />
                       </div>
                     )}
@@ -142,13 +167,7 @@ export const AddSocialPostModal = ({ open, handleClose }) => {
 
               <div className="flex justify-between px-4 py-4">
                 <button
-                  className="bg-white hover:bg-green-700 text-green-500 px-4 py-2 rounded border-2 border-green-500"
-                  onClick={handleClose}
-                >
-                  Cancel
-                </button>
-                <button
-                  className="bg-green-500 hover:bg-green-700 text-white px-4 py-2 rounded"
+                  className="bg-green-500 hover:bg-green-700 text-white px-4 py-2 rounded w-full"
                   type="submit"
                 >
                   Upload
