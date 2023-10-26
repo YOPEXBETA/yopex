9723a71e34f0820f971cf48581ed5c3710342e57
Mouhib Touaiti <mouhib.touaiti@esprit.tn>
2023-10-26 17:21:32 +0100

    updates

--- client/src/Components/shared/Modals/JobOfferModal.jsx
+++ client/src/Components/shared/Modals/JobOfferModal.jsx
@@ -15,7 +15,6 @@ const JobOfferModal = ({ open, handleClose, job }) => {
   // React-hook-form
   const onclick = () => {
     applyToJob();
-    handleClose();
   };
 
   return (
@@ -44,51 +43,66 @@ const JobOfferModal = ({ open, handleClose, job }) => {
               />
             </svg>
           </button>
-          <div className="bg-white dark:bg-zinc-800 rounded-lg shadow-xl md:w-5/6 md:h-5/6 h-screen w-screen overflow-y-auto max-h-full">
+          <div className="bg-white dark:bg-zinc-800 rounded-lg shadow-xl md:w-5/6 md:h-5/6 border h-screen w-screen overflow-y-auto max-h-full">
             <div className="grid grid-cols-12 gap-4">
-              <div className="col-span-12 sm:col-span-8 md:col-span-12 lg:col-span-8 xl:col-span-8">
-                {" "}
-                <div className="p-4 border col-span-8 sm:col-span-4">
-                  <div className="p-8">
-                    <div className="flex items-center justify-between">
-                      <div className="flex items-center gap-4">
-                        <img
-                          src={job?.company?.companyLogo}
-                          alt="Icon"
-                          className="w-16 h-16 rounded-lg object-cover"
-                        />
-                        <div>
-                          <p className="text-lg font-bold dark:text-gray-200">
-                            {job.title}
-                          </p>
-                          <p className="dark:text-gray-200">
-                            By {job?.company?.companyName}
-                          </p>
+              <div className="col-span-12 sm:col-span-8 md:col-span-12 lg:col-span-12 xl:col-span-12">
+                <div className="flex flex-col items-center justify-center gap-4 mt-8">
+                  <img
+                    src={job?.company?.companyLogo}
+                    alt="Icon"
+                    className="w-24 h-24 rounded-lg object-cover"
+                  />
+                  <div className="flex flex-col justify-center items-center">
+                    <p className="text-2xl font-bold dark:text-gray-200 mb-2">
+                      {job.title}
+                    </p>
+                    <p className="dark:text-gray-200">
+                      By {job?.company?.companyName}
+                    </p>
+                  </div>
+                  <div className="flex dark:bg-zinc-800 items-center bg-white">
+                    <Link to={`/company/${job.company._id}`}>
+                      <button className="bg-green-500 hover:bg-green-700 text-white px-4 py-2 rounded">
+                        <div className="flex items-center justify-center">
+                          <FaEye className="mr-2" />
+                          <p>View Profile</p>
                         </div>
-                      </div>
+                      </button>
+                    </Link>
+                    <div className="flex dark:bg-zinc-800 justify-between px-4 py-2 bg-white">
+                      {user && !user.companies.includes(job.company._id) && (
+                        <button
+                          className="bg-purple-500 hover:bg-purple-700 text-white px-4 py-2 rounded"
+                          onClick={onclick}
+                        >
+                          <div className="flex items-center justify-center">
+                            <p className="mr-2">Apply Now</p>
+                            <IoIosArrowForward />
+                          </div>
+                        </button>
+                      )}
                     </div>
+                  </div>
+                </div>
 
-                    <hr className="my-4 border-t" />
+                <hr className="mt-8 border-b" />
+              </div>
+
+              <div className="col-span-12 sm:col-span-8 md:col-span-12 lg:col-span-8 xl:col-span-8">
+                <div className="p-4 col-span-8 sm:col-span-4">
+                  <div className="px-8">
                     <div>
-                      <p className="text-md font-semibold dark:text-gray-200 mb-4">
+                      <p className="text-xl font-semibold dark:text-gray-200 mb-4">
                         Job Description
                       </p>
                       <div
                         className="text-md dark:text-white mb-4"
+                        style={{ wordWrap: "break-word" }}
                         dangerouslySetInnerHTML={{ __html: job?.description }}
                       />
                     </div>
                     <hr className="my-4 border-t" />
-                    <div className="flex justify-between">
-                      <p className="text-md font-semibold dark:text-gray-200">
-                        Posted time
-                      </p>
-                      <p className="text-md font-normal text-green-500">
-                        {formatDistance(new Date(job?.createdAt), new Date(), {
-                          addSuffix: true,
-                        })}
-                      </p>
-                    </div>
+
                     <hr className="my-4 border-t" />
                     <div className="flex flex-col justify-between my-4">
                       <p className="text-md font-semibold dark:text-gray-200 mb-2">
@@ -136,43 +150,25 @@ const JobOfferModal = ({ open, handleClose, job }) => {
                   </div>
                 </div>
               </div>
-              <div className="hidden lg:block  border md:col-span-0  lg:col-span-4 xl:col-span-4">
-                <div className="flex flex-col m-8">
-                  {/*<h2 className="text-left">Job Overview</h2>*/}
-                </div>
-                <div className="flex flex-col items-center justify-center gap-4 mt-8">
-                  <img
-                    src={job?.company?.companyLogo}
-                    alt="Icon"
-                    className="w-16 h-16 rounded-lg object-cover"
-                  />
-                  <div>
-                    <p className="dark:text-gray-200 font-bold">
-                      {job?.company?.companyName}
+              <div className="hidden lg:block md:col-span-0 lg:col-span-4 xl:col-span-4 ">
+                <div className="border mx-8 rounded-lg">
+                  <div className="flex flex-col   rounded-lg px-8 py-2">
+                    <h2 className="text-left dark:text-white font-semibold text-gray-500">
+                      Appliers
+                    </h2>
+                    <p className="dark:text-white font-bold">
+                      {job?.appliers?.length} applier
                     </p>
                   </div>
-                  <div className="flex flex-col dark:bg-zinc-800 items-center bg-white">
-                    <Link to={`/company/${job.company._id}`}>
-                      <button className="bg-green-500 hover:bg-green-700 text-white px-4 py-2 rounded w-80">
-                        <div className="flex items-center justify-center">
-                          <FaEye className="mr-2" />
-                          <p>View Profile</p>
-                        </div>
-                      </button>
-                    </Link>
-                    <div className="flex dark:bg-zinc-800 justify-between px-4 py-2 bg-white">
-                      {user && !user.companies.includes(job.company._id) && (
-                        <button
-                          className="bg-purple-500 hover:bg-purple-700 text-white px-4 py-2 rounded w-80"
-                          onClick={onclick}
-                        >
-                          <div className="flex items-center justify-center">
-                            <p className="mr-2">Apply Now</p>
-                            <IoIosArrowForward />
-                          </div>
-                        </button>
-                      )}
-                    </div>
+                  <div className="flex flex-col  rounded-lg px-8 py-2">
+                    <h2 className="text-left dark:text-white font-semibold text-gray-500">
+                      Job Posted
+                    </h2>
+                    <p className="dark:text-white font-bold">
+                      {formatDistance(new Date(job?.createdAt), new Date(), {
+                        addSuffix: true,
+                      })}
+                    </p>
                   </div>
                 </div>
               </div>
