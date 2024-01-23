c42e6c15d42490f95c76010c6af05ca8ee4e7e29
Mouhib Touaiti <mouhib.touaiti@esprit.tn>
2024-01-23 11:07:50 +0100

    update ui

--- client/src/Components/icons/ArrowIcon.jsx
+++ client/src/Components/icons/ArrowIcon.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ArrowIcon = () => {
   return (
-    <div className="dark:text-white">
+    <div className="hover:text-white">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         className="w-5 h-5 shrink-0"
