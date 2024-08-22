69e671fb29329e226d09ecceff466d50e72c02cb
BahaEddinDridi <bahaeddine170@gmail.com>
2024-08-22 17:23:22 +0100

    feat: users can now add Emojis to their messages

--- client/src/Pages/UserDashboard/ContestDescriptionPage/leftSide/Components/TeamChallengeConversations/TeamChallengeConversation.jsx
+++ client/src/Pages/UserDashboard/ContestDescriptionPage/leftSide/Components/TeamChallengeConversations/TeamChallengeConversation.jsx
@@ -3,6 +3,7 @@ import {format, formatDistance} from "date-fns";
 import React, { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import { io } from "socket.io-client";
+import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
 import AvatarProfile from "../../../../../../assets/images/AvatarProfile.jpg";
 import {
     useCreateTeamChallengeMessage,
@@ -11,10 +12,17 @@ import {
 } from "../../../../../../hooks/react-query/useTeamChallengeConversations";
 import ChevronUpIcon from "../../../../../../Components/icons/ChevronUpIcon";
 import ChevronDownIcon from "../../../../../../Components/icons/ChevronDownIcon";
+import ChevronLeftIcon from "../../../../../../Components/icons/ChevronLeftIcon";
+import InfoIcon from "../../../../../../Components/icons/InfoIcon";
+import ChatInfoIcon from "../../../../../../Components/icons/ChatInfoIcon";
+import EmojiPicker from "emoji-picker-react";
+import EmojiIcon from "../../../../../../Components/icons/EmojiIcon";
 
 const TeamChallengeConversation = ({ challenge }) => {
     const chatContainerRef = useRef(null);
     const [membersVisible, setMembersVisible] = useState(false);
+    const [sidebarVisible, setSidebarVisible] = useState(true);
+    const [showEmojiPicker, setShowEmojiPicker] = useState(false);
     const { data: teamChallengeConversation } = useGetTeamChallengeConversation(challenge?._id);
     const [members, setMembers] = useState([]);
     const conversationId = teamChallengeConversation?._id;
@@ -24,6 +32,7 @@ const TeamChallengeConversation = ({ challenge }) => {
     const [message, setMessage] = useState("");
     const [arrivalMessage, setArrivalMessage] = useState([]);
     const [socket, setSocket] = useState(null);
+    const emojiPickerRef = useRef(null);
 
     if (chatContainerRef.current !== null)
         chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
@@ -57,7 +66,17 @@ const TeamChallengeConversation = ({ challenge }) => {
             setMembers(teamChallengeConversation.members);
         }
     }, [teamChallengeConversation]);
-
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (emojiPickerRef.current && !emojiPickerRef.current.contains(event.target)) {
+                setShowEmojiPicker(false);
+            }
+        };
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
     const handleCreateMessage = async (event) => {
         event.preventDefault();
 
@@ -75,11 +94,29 @@ const TeamChallengeConversation = ({ challenge }) => {
         mutate({ conversationId: conversationId, message, sender: user._id });
         setMessage("");
     };
-
+    const handleEmojiClick = (emoji) => {
+        setMessage((prev) => prev + emoji.emoji);
+    };
     return (
-        <div className="container mx-auto shadow-lg bg-white rounded-lg grid grid-cols-3 gap-4">
-            <div className="col-span-2 flex flex-col justify-between">
-                <div className="flex flex-col mt-5">
+        <div className="container mx-auto shadow-lg bg-white rounded-lg grid grid-cols-3">
+            <div className={`col-span-${sidebarVisible ? "2" : "3"} flex flex-col justify-between`}>
+                <div className="flex flex-col">
+                    <div className="flex items-center bg-white rounded-t-lg shadow-md p-4 border-b border-gray-300">
+                        <img
+                            src={user.picturePath || AvatarProfile}
+                            className="object-cover h-10 w-10 rounded-full"
+                            alt={user.firstname}
+                        />
+                        <div className="ml-3 flex-grow">
+                            <p className="font-semibold text-lg">{user.firstname} {user.lastname}</p>
+                        </div>
+                        <div
+                            className="ml-3 cursor-pointer"
+                            onClick={() => setSidebarVisible(!sidebarVisible)}
+                            title="Chat Information">
+                            <ChatInfoIcon/>
+                        </div>
+                    </div>
                     {arrivalMessage?.length === 0 ? (
                         <div
                             ref={chatContainerRef}
@@ -98,111 +135,115 @@ const TeamChallengeConversation = ({ challenge }) => {
                             {arrivalMessage?.map((message, index) => (
                                 <div
                                     key={index}
-                                    className={`flex items-end ${
-                                        message.sender._id === user._id
-                                            ? "justify-end"
-                                            : "justify-start"
-                                    } mb-4`}
+                                    className={`flex flex-col items-${message.sender._id === user._id ? "end" : "start"} mb-4`}
                                 >
-                                    {message.sender._id !== user._id && (
-                                        <img
-                                            src={
-                                                message.sender.picturePath ||
-                                                AvatarProfile
-                                            }
-                                            className="object-cover h-8 w-8 rounded-full shadow-md "
-                                            alt={message.sender.firstname}
-                                            title={message.sender.firstname}
-                                        />
-                                    )}
-                                    <div
-                                        className={`ml-2 py-3 px-4 max-w-xs md:max-w-md lg:max-w-lg ${
-                                            message.sender._id === user._id
-                                                ? "bg-green-500  rounded-l-2xl rounded-tr-lg"
-                                                : "bg-gray-200  rounded-tl-lg rounded-r-2xl"
-                                        }  text-black break-words shadow-md`}
-                                        style={{wordWrap: "break-word"}}
-                                    >
-                                        <p>{message.message}</p>
-                                        <p className="text-black text-[0.75rem]">
-                                            {formatDistance(
-                                                new Date(message.createdAt),
-                                                new Date(),
-                                                {addSuffix: true}
-                                            )}
-                                        </p>
+                                    <div className="flex items-end">
+                                        {message.sender._id !== user._id && (
+                                            <img
+                                                src={message.sender.picturePath || AvatarProfile}
+                                                className="object-cover h-8 w-8 rounded-full shadow-md"
+                                                alt={message.sender.firstname}
+                                                title={message.sender.firstname}
+                                            />
+                                        )}
+                                        <div
+                                            className={`ml-2 py-3 px-4 max-w-xs md:max-w-md lg:max-w-lg ${
+                                                message.sender._id === user._id
+                                                    ? "bg-green-500 rounded-l-2xl rounded-tr-lg"
+                                                    : "bg-gray-200 rounded-tl-lg rounded-r-2xl"
+                                            } text-black break-words shadow-md`}
+                                            style={{wordWrap: "break-word"}}
+                                        >
+                                            <p>{message.message}</p>
+                                        </div>
                                     </div>
+                                    <p className="text-black text-[0.75rem] mt-1">
+                                        {formatDistance(new Date(message.createdAt), new Date(), {addSuffix: true})}
+                                    </p>
                                 </div>
                             ))}
                         </div>
                     )}
                 </div>
                 <div className="py-5 px-2">
-                    <form onSubmit={handleCreateMessage} className="flex items-center">
+                    <form onSubmit={handleCreateMessage} className="flex items-center relative">
                         <input
                             value={message}
                             onChange={(event) => setMessage(event.target.value)}
-                            className="w-full bg-gray-300 py-2 px-3 rounded-l-xl "
+                            className="w-full bg-gray-100 py-2 px-3 rounded-l-xl"
                             type="text"
                             placeholder="type your message here..."
                         />
                         <button
+                            type="button"
+                            onClick={() => setShowEmojiPicker((prev) => !prev)}
+                            className="absolute end-28 top-1/2 transform -translate-y-1/2 text-gray-600"
+                        >
+                            <EmojiEmotionsIcon/>
+                        </button>
+                        <button
                             type="submit"
-                            className="bg-green-500 text-white flex items-center px-4 py-2  rounded-r-xl hover:bg-green-600"
+                            className="bg-green-500 text-white flex items-center px-4 py-2 rounded-r-xl hover:bg-green-600"
                         >
                             <span className="mr-2">Send</span>
                             <SendIcon/>
                         </button>
-                    </form>
-                </div>
-
-            </div>
-
-            <div className="col-span-1 border-l border-gray-400">
-                <div className="flex text-center pt-2 items-center flex-col">
-                    <img
-                        src={challenge.picturePath}
-                        className="object-cover h-24 w-24  rounded-full shadow-md"
-                        alt=""
-                    />
-                    <div className="font-semibold pt-4">General Challenge Conversation</div>
-                    <div className=" py-2">Created {format(new Date(challenge.createdAt), "dd MMM yyyy")}</div>
-                    <div className="w-full dark:bg-gray-800 rounded-lg">
-                        <button
-                            className="flex gap-12 items-center justify-center w-11/12 mx-auto text-gray-900 dark:text-gray-400 hover:bg-gray-200 dark:bg-gray-700 p-2 rounded-lg"
-                            onClick={() => setMembersVisible(!membersVisible)}
-                        >
-
-                            <span className="">Chat Members</span>
-                            {membersVisible ? (
-                                <ChevronUpIcon className="ml-2 " />
-                            ) : (
-                                <ChevronDownIcon className="ml-2" />
-                            )}
-                        </button>
-                        {membersVisible && (
-                            <div className="p-4">
-                                <ul>
-                                    {members.map((member) => (
-                                        <li key={member._id} className="flex items-center mb-2">
-                                            <img
-                                                alt={member.firstname}
-                                                src={member.picturePath || AvatarProfile}
-                                                className="h-10 w-10 rounded-full mr-3"
-                                            />
-                                            <div>
-                                                <p className="text-gray-800 dark:text-gray-200 text-start font-medium">
-                                                    {member.firstname} {member.lastname}
-                                                </p>
-                                            </div>
-                                        </li>
-                                    ))}
-                                </ul>
+                        {showEmojiPicker && (
+                            <div ref={emojiPickerRef}
+                                 className="absolute bottom-full right-0 z-50">
+                                <EmojiPicker onEmojiClick={handleEmojiClick}/>
                             </div>
                         )}
-                    </div>
+                    </form>
                 </div>
             </div>
+
+            {sidebarVisible && (
+                <div className="col-span-1 border-l border-gray-400 flex flex-col">
+                    <div className="flex text-center pt-2 items-center flex-col">
+                        <img
+                            src={challenge.picturePath}
+                            className="object-cover h-24 w-24 rounded-full shadow-md"
+                            alt=""
+                        />
+                        <div className="font-semibold pt-4">General Challenge Conversation</div>
+                        <div className="py-2">Created {format(new Date(challenge.createdAt), "dd MMM yyyy")}</div>
+                        <div className="w-full dark:bg-gray-800 rounded-lg">
+                            <button
+                                className="flex gap-12 items-center justify-center w-11/12 mx-auto text-gray-900 dark:text-gray-400 hover:bg-gray-200 dark:bg-gray-700 p-2 rounded-lg"
+                                onClick={() => setMembersVisible(!membersVisible)}
+                            >
+                                <span>Chat Members</span>
+                                {membersVisible ? (
+                                    <ChevronUpIcon className="ml-2"/>
+                                ) : (
+                                    <ChevronDownIcon className="ml-2"/>
+                                )}
+                            </button>
+                            {membersVisible && (
+                                <div className="p-4">
+                                <ul>
+                                        {members.map((member) => (
+                                            <li key={member._id} className="flex items-center mb-2">
+                                                <img
+                                                    alt={member.firstname}
+                                                    src={member.picturePath || AvatarProfile}
+                                                    className="h-10 w-10 rounded-full mr-3"
+                                                />
+                                                <div>
+                                                    <p className="text-gray-800 dark:text-gray-200 text-start font-medium">
+                                                        {member.firstname} {member.lastname}
+                                                    </p>
+                                                </div>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
