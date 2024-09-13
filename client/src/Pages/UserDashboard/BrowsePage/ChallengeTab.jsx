cfd088230ac7fc632b69da1ce8d8e0143c137083
Mouhib Touaiti <mouhib.touaiti.tm@gmail.com>
2024-09-13 17:17:57 +0100

    update challenge page ui

--- client/src/Pages/UserDashboard/BrowsePage/ChallengeTab.jsx
+++ client/src/Pages/UserDashboard/BrowsePage/ChallengeTab.jsx
@@ -1,28 +1,43 @@
 import React from "react";
 import IndividualIcon from "../../../Components/icons/IndividualIcon";
 import TeamsIcon from "../../../Components/icons/TeamIcon";
+import SearchIcon from "../../../Components/icons/SearchIcon";
 
 
-const ChallengeTab = ({ changeValue, value }) => {
+const ChallengeTab = ({ changeValue, value,setContestQuery }) => {
   return (
-      <div className="flex flex-col items-center sm:flex-row sm:items-center rounded-full text-gray-700 dark:bg-zinc-900 dark:text-white">
-        <div className="flex flex-wrap gap-2">
+    <div>
+    <div className="flex flex-col items-center sm:flex-row sm:items-center rounded-full text-zinc-700 dark:bg-zinc-900 dark:text-white">
+      <div className="relative w-full">
+        <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+          <div className="pointer-events-none">
+            <SearchIcon />
+          </div>
+        </div>
+        
+          <input
+            type="text"
+            className="border border-gray-200 rounded-full border-gray-300 block w-full pl-10 p-2.5 dark:bg-zinc-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white hover:border-green-500"
+            placeholder="Search for challenges..."
+            onChange={(e) => setContestQuery(e.currentTarget.value)}
+            />      
+        <div className="flex absolute inset-y-0 right-0 items-center pr-0  bg-zinc-200 rounded-full">
           <button
-              className={`flex items-center rounded-full transition-colors duration-300 ease-in focus:outline-none hover:text-indigo-500 focus:text-indigo-500 px-4 py-2 ${
-                  value === 0 ? "bg-white shadow dark:bg-zinc-700 dark:text-white" : ""
-              }`}
-              onClick={() => changeValue(0)}
-          >
+            className={`flex items-center rounded-full dark:text-black transition-colors duration-300 ease-in focus:outline-none hover:text-indigo-500 focus:text-indigo-500 px-4 py-2 ${
+              value === 0 ? "bg-white shadow" : ""
+            }`}
+            onClick={() => changeValue(0)}
+            >
             <div className="flex gap-2">
-              <IndividualIcon />
-              <span className="hidden sm:inline">Individuals</span>
+            <IndividualIcon />
+            <span className="hidden sm:inline">Individuals</span>
             </div>
           </button>
           <button
-              className={`flex items-center rounded-full transition-colors duration-300 ease-in focus:outline-none hover:text-indigo-500 focus:text-indigo-500 px-4 py-2 ${
-                  value === 1 ? "bg-white shadow dark:bg-zinc-700 dark:text-white" : ""
-              }`}
-              onClick={() => changeValue(1)}
+            className={`flex items-center rounded-full dark:text-black transition-colors duration-300 ease-in focus:outline-none hover:text-indigo-500 focus:text-indigo-500 px-4 py-2 ${
+              value === 1 ? "bg-white shadow" : ""
+            }`}
+            onClick={() => changeValue(1)}
           >
             <div className="flex gap-2">
               <TeamsIcon />
@@ -31,6 +46,8 @@ const ChallengeTab = ({ changeValue, value }) => {
           </button>
         </div>
       </div>
+    </div>
+    </div>
   );
 };
 
--- client/src/Pages/UserDashboard/BrowsePage/components/ContestsHeader.jsx
+++ client/src/Pages/UserDashboard/BrowsePage/components/ContestsHeader.jsx
@@ -3,6 +3,7 @@ import SearchIcon from "../../../../Components/icons/SearchIcon";
 import FilterIcon from "../../../../Components/icons/FilterIcon";
 import ChallengeFilterModal from "../../../../Components/Modals/ChallengeFilterModal";
 import ChallengeFilterPanel from "./ChallengeFilterPanel";
+import ChallengeTab from "../ChallengeTab";
 
 const ContestsHeader = ({
   setContestQuery,
@@ -12,37 +13,14 @@ const ContestsHeader = ({
   setCategoryQuery,
   selectedCategory,
   selectedSkill,
+  changeValue, 
+  value 
 }) => {
   const [openPostModal, setOpenPostModal] = useState(false);
   const [isFilterPanelOpen, setIsFilterPanelOpen] = useState(false);
   const toggleFilterPanel = () => setIsFilterPanelOpen((prev) => !prev);
   return (
       <div className="relative"> {/* Added relative positioning here */}
-        <div className="flex items-center">
-          <label htmlFor="voice-search" className="sr-only">
-            Search
-          </label>
-          <div className="relative w-full">
-            <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-              <SearchIcon/>
-            </div>
-            <input
-                type="text"
-                onChange={(e) => setContestQuery(e.currentTarget.value)}
-                className="border rounded-full border-gray-300 block w-full pl-10 p-2.5 dark:bg-zinc-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white hover:border-green-500"
-                placeholder="Search for Challenges"
-            />
-          </div>
-          <button
-              onClick={toggleFilterPanel}
-              type="button"
-              className="inline-flex items-center py-2.5 px-3 ml-2 text-sm font-medium text-white bg-black rounded-full border hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-green-500 dark:hover:bg-green-700 dark:focus:ring-blue-800"
-          >
-            <FilterIcon/>
-            Filters
-          </button>
-        </div>
-
         {/* Render the ChallengeFilterPanel as a dropdown */}
         {isFilterPanelOpen && (
             <ChallengeFilterPanel
@@ -55,6 +33,34 @@ const ContestsHeader = ({
                 handleClose={toggleFilterPanel}
             />
         )}
+
+    <div className="flex justify-between items-center mb-5">
+        
+        <h5 className="my-5 text-xl text-left dark:text-white">
+          Challenges ( )
+        </h5>
+        {/* Render the ChallengeFilterPanel as a dropdown */}
+        {isFilterPanelOpen && (
+            <ChallengeFilterPanel
+                setMinAmount={setMinAmount}
+                setMaxAmount={setMaxAmount}
+                setSkillQuery={setSkillQuery}
+                setCategoryQuery={setCategoryQuery}
+                selectedCategory={selectedCategory}
+                selectedSkill={selectedSkill}
+                handleClose={toggleFilterPanel}
+            />
+        )}
+
+          <button
+              onClick={toggleFilterPanel}
+              type="button"
+              className="inline-flex items-center py-2.5 px-3 ml-2 text-sm font-medium text-white bg-black rounded-full border hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-green-500 dark:hover:bg-green-700 dark:focus:ring-blue-800"
+          >
+            <FilterIcon/>
+            Filters
+          </button>
+        </div>
       </div>
   );
 };
